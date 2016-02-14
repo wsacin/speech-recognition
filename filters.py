27b714732cf3fb760bec52d50bba817d820a8661
walter <wsa@cin.ufpe.br>
2016-02-14 22:34:44 +0000

    Added module with custom butterworh filter.

---
+++ filters.py
@@ -0,0 +1,13 @@
+from scipy.signal import butter, lfilter
+
+def butter_bandstop(self, lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, low, btype='highpass')
+    return b, a
+
+def butter_bandstop_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandstop(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
